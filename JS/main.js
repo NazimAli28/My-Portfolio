fded0cb0190c724fff74cde322de3ad81ecead14
NazimAli28 <nazimalionline@gmail.com>
2022-12-28 19:42:47 +0500

    update

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,13 @@
 
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//         var scroll = $(window).scrollTop();
-//         if (scroll > 100) {
-//             $(".navbar").css("background", "red");
-//         }
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 100) {
+            $(".navbar").css("background", "red");
+        }
 
-//         else {
-//             $(".navbar").css("background", "transparent");
-//         }
-//     })
-// })
-
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 50) {
-        nav.classList.add('active_nav');
-    } else {
-        nav.classList.remove('active_nav');
-    }
+        else {
+            $(".navbar").css("background", "transparent");
+        }
+    })
 })
